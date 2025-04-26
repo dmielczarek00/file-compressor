15d8450b4e9c40a58dc4fe3b87616a950e5217e6
Creeperasek <76570152+Creeperasek@users.noreply.github.com>
2025-04-26 22:00:32 +0200

    Fix: Fixed no file found

--- backend/worker.py
+++ backend/worker.py
@@ -144,6 +144,8 @@ class CompressionWorker:
                     "message": message
                 }
 
+                logger.info(f"op: {output_path} -- f_p {file_path} --- {result}")
+
                 await self.db.update_job_status(job_id, 'finished')
                 logger.info(f"Job {job_id} completed successfully")
             else:
